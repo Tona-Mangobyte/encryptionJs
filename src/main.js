038c28ed5c09b2aa7b2b2f22078efb17b283f9b2
Tona <tona@mango-byte.com>
2022-02-19 15:13:42 +0700

    init encryptionJs

---
+++ src/main.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var crypto_js_1 = require("crypto-js");
+console.log(crypto_js_1.SHA256('message'));
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import * as crypto from 'crypto-js';
+import { v4 as UUID } from 'uuid';
+
+console.log(crypto.SHA256('message').toString()); // ab530a13e45914982b79f9b7e3fba994cfd1f3fb22f71cea1afbf02b460c6d1d
+console.log(crypto.SHA256(UUID()).toString()); // Random unique string
